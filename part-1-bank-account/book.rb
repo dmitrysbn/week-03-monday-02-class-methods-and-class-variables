f2e328bf7bd7234e2929893ea9d7023d63f1a2c4
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-05 14:35:49 -0500

    add available, borrowed, browse methods to Book

--- part-1-bank-account/book.rb
+++ part-1-bank-account/book.rb
@@ -4,8 +4,10 @@ class Book
   @@on_loan = []
 
   ## CLASS METHODS ##
-  def self.create
-
+  def self.create(title, author, isbn)
+    book = Book.new(title, author, isbn)
+     @@on_shelf << book
+    return book
   end
 
   def self.current_due_date
@@ -17,20 +19,22 @@ class Book
   end
 
   def self.browse
-
+    @@on_shelf[rand(@@on_shelf.size)]
   end
 
   def self.available
-
+    @@on_shelf
   end
 
   def self.borrowed
-
+    @@on_load
   end
 
   ## INSTANCE METHODS ##
-  def initialize
-
+  def initialize(title, author, isbn)
+    @title = title
+    @author = author
+    @isbn = isbn
   end
 
   def borrow
@@ -52,3 +56,15 @@ class Book
   def due_date=(due_date)
     @due_date = due_date
   end
+end
+
+book1 = Book.create(1,1,5)
+book2 = Book.create(1,1,5)
+book3 = Book.create(1,1,5)
+book4 = Book.create(1,1,5)
+
+# puts Book.available
+
+puts Book.browse
+puts Book.browse
+puts Book.browse
