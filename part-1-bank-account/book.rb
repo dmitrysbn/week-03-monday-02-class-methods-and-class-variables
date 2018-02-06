fda8356ce087e9d97a4faec07e6a4953be165d32
dmitrysbn <dmitry.sbn@gmail.com>
2018-02-05 21:04:55 -0500

    add tests to Book

--- part-1-bank-account/book.rb
+++ part-1-bank-account/book.rb
@@ -13,11 +13,13 @@ class Book
 
   # The due date is 21 days from the lend date.
   def self.current_due_date
-    @@current_due_date = (Time.now + (3600*24*21)).asctime
+    @@current_due_date = Time.now + (3600*24*21)
   end
 
-  def self.overdue_books
-
+  def self.overdue
+    @@on_loan.select do |book|
+      book.due_date < Time.now
+    end
   end
 
   def self.browse
@@ -104,10 +106,10 @@ class Book
   end
 end
 
-book1 = Book.create(1,1,5)
-book2 = Book.create(1,1,5)
-book3 = Book.create(1,1,5)
-book4 = Book.create(1,1,5)
+# book1 = Book.create(1,1,5)
+# book2 = Book.create(1,1,5)
+# book3 = Book.create(1,1,5)
+# book4 = Book.create(1,1,5)
 
 # puts Book.available
 
@@ -115,10 +117,33 @@ book4 = Book.create(1,1,5)
 # puts Book.browse
 # puts Book.browse
 
-puts book4.lent_out?
-puts book4.borrow
-puts book4.due_date
-puts book4.lent_out?
-puts book4.return_to_library
-puts book4.due_date
-puts Book.available
+# puts book4.lent_out?
+# puts book4.borrow
+# puts book4.due_date
+# puts book4.lent_out?
+# puts book4.return_to_library
+# puts book4.due_date
+# puts Book.available
+# puts Book.overdue.inspect
+
+## TEST INPUTS FROM ASSIGNMENT ##
+sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
+aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
+if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+puts Book.browse.inspect # #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.available.inspect # [#<Book:0x00555e82acde20 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431">, #<Book:0x00555e82acdce0 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+puts Book.borrowed.inspect # []
+puts sister_outsider.lent_out? # false
+puts sister_outsider.borrow # true
+puts sister_outsider.lent_out? # true
+puts sister_outsider.borrow # false
+puts sister_outsider.due_date # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+puts Book.borrowed.inspect # [#<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=2017-02-25 20:55:17 -0500>]
+puts Book.overdue.inspect # []
+puts sister_outsider.return_to_library # true
+puts sister_outsider.lent_out? # false
+puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
+puts Book.borrowed.inspect # []
